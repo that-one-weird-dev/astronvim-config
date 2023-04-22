e5b5907d4d11c1949780d94ab5073fca7ff83ba7
manu <manuelugoccioni@gmail.com>
2023-04-22 19:57:57 +0200

    Basic configuration

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,10 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+          require("astronvim.utils.buffer").close(
+            bufnr)
+        end)
       end,
       desc = "Pick to close",
     },
@@ -19,7 +22,9 @@ return {
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<leader>fs"] = { ":w!<cr>", desc = "Save file" },
+    ["<Tab>"] = { "]b", desc = "Next buffer", remap = true },
+    ["<S-Tab>"] = { "[b", desc = "Next buffer", remap = true }
   },
   t = {
     -- setting a mapping to false will disable it
